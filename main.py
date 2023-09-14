b23e4aaa7f62ed781f4695f6b28c330137dac08f
swarna0612 <144356463+swarna0612@users.noreply.github.com>
2023-09-14 12:50:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-#factorial of a number using recursion
-def recur_factorial(n):
-    if n==1:
-        return n
-    else:
-        return n*recur_factorial(n-1)
-num=int(input("enter a factorial number:"))
-#check if the number is negative
-if num<0:
-   print("sorry,factorial does not exist for negative numbers")
-elif num==0:
-   print("the factorial of 0 is 1")
-else:
-   print("the factorial of",num,"is",recur_factorial(num))
-      
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than 0.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.__account_balance:
+                self.__account_balance -= amount
+                print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+            else:
+                print("Insufficient funds.")
+        else:
+            print("Invalid withdrawal amount. Amount must be greater than 0.")
+
+    def display_balance(self):
+        print(f"Account balance for {self.__account_holder_name} (Account #{self.__account_number}): ${self.__account_balance}")
+
+
+# Testing the BankAccount class
+if __name__ == "__main__":
+    # Create a bank account
+    account = BankAccount("12345", "John Doe", 1000.0)
+
+    # Display initial balance
+    account.display_balance()
+
+    # Make a deposit
+    account.deposit(500.0)
+
+    # Make a withdrawal
+    account.withdraw(200.0)
+
+    # Try to withdraw more than the balance
+    account.withdraw(1500.0)
+
+    # Display the updated balance
+    account.display_balance()
